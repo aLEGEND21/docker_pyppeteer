271099010f74b65d6b908d65851facd9ab4f86a2
aLEGEND21 <arnavmurthi@gmail.com>
2022-11-22 16:23:51 -0500

    Change viewport and screenshot size

--- app/main.py
+++ app/main.py
@@ -6,14 +6,14 @@ async def main():
     async with async_playwright() as p:
         ticker = "AllUSA"
         browser = await p.chromium.launch()
-        browser._defaultViewport = {"width": 1200, "height": 1000} # Increase viewport height so that more stocks are visible
         page = await browser.new_page()
+        await page.set_viewport_size({"width": 1200, "height": 1000}) # Increase viewport height so that more stocks are visible
         url = f"https://www.tradingview.com/heatmap/stock/?color=change&dataset={ticker}&group=sector&size=market_cap_basic&theme=dark"
         await page.goto(url, wait_until="networkidle") # Wait for most background tasks to be done
         x_button = await page.query_selector(".tv-dialog__close")
         if x_button is not None:
             await x_button.click()
-        await asyncio.sleep(0.5) # Wait for the dialog to close
+            await page.wait_for_timeout(100) # Wait for the banner to disappear
         heatmap_top_bar = await page.query_selector('div[class^="heatmapTopBar"]')
         heatmap = await page.query_selector("canvas")
         full_box = bar_box = await heatmap_top_bar.bounding_box()
